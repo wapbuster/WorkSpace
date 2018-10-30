0660e81cf7bec27bdee99dac2bd399b234df5ab7
WapBuster <wapbuster@gmail.com>
2018-10-29 20:54:32 -0400

    ws

---
+++ WorkSpace.Home/Constructors/Calc/src/main/java/com/indus/training/domain/CalciInput.java
@@ -0,0 +1,23 @@
+package com.indus.training.domain;
+
+public class CalciInput {
+
+	private double param1;
+
+	private double param2;
+
+	public CalciInput(double param1, double param2) {
+
+		this.param1 = param1;
+		this.param2 = param2;
+	}
+
+	public double getParam1() {
+		return param1;
+	}
+
+	public double getParam2() {
+		return param2;
+	}
+
+}
